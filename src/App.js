ff3d9d256d6d07cfd9de4c8e9b2521cd06a241c7
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-23 13:43:59 -0400

    internal card flex

--- src/App.js
+++ src/App.js
@@ -18,10 +18,7 @@ function App() {
             <span>Rig Veda are creatures of the cosmos from which we spring with pretty stories 
               for which there's little good evidence prime number Vangelis? Another world finite
               but unbounded shores of the cosmic ocean invent the universe hearts of the stars two 
-              ghostly white figures in coveralls and helmets are softly dancing? Concept of the 
-              number one a still more glorious dawn awaits bits of moving fluff take root and flourish 
-              dream of the mind's eye something incredible is waiting to be known and billions upon billions 
-              upon billions upon billions upon billions upon billions upon billions.</span>
+              ghostly white figures in coveralls and helmets are softly dancing?.</span>
           </div>
 
           <div className="exposition">
@@ -30,10 +27,7 @@ function App() {
               Rig Veda vanquish the impossible rich in mystery muse about. Tendrils of gossamer 
               clouds rings of Uranus invent the universe the sky calls to us two ghostly white 
               figures in coveralls and helmets are softly dancing tendrils of gossamer clouds?
-              Circumnavigated something incredible is waiting to be known made in the interiors 
-              of collapsing stars as a patch of light the carbon in our apple pies the carbon in 
-              our apple pies and billions upon billions upon billions upon billions upon billions 
-              upon billions upon billions.</span>
+              </span>
           </div>
 
           <footer>
