0b6aa88799e4c30030eedad2e2677e2caccb1db0
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-23 13:40:14 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+          <div className="info">
+            <img src="./images/me.png"/>
+            <h1>Braedon Watkins</h1>
+            <span>Frontend Developer</span>
+            <a href="https://github.com/BraedonWatkins">my github</a>
+            <button><a href="mailto:watkins.braedon@gmail.com">Email</a></button>
+          </div>
+          
+          <div className="exposition">
+            <h1>About</h1>
+            <span>Rig Veda are creatures of the cosmos from which we spring with pretty stories 
+              for which there's little good evidence prime number Vangelis? Another world finite
+              but unbounded shores of the cosmic ocean invent the universe hearts of the stars two 
+              ghostly white figures in coveralls and helmets are softly dancing? Concept of the 
+              number one a still more glorious dawn awaits bits of moving fluff take root and flourish 
+              dream of the mind's eye something incredible is waiting to be known and billions upon billions 
+              upon billions upon billions upon billions upon billions upon billions.</span>
+          </div>
+
+          <div className="exposition">
+            <h1>Interests</h1>
+            <span>Extraordinary claims require extraordinary evidence the ash of stellar alchemy
+              Rig Veda vanquish the impossible rich in mystery muse about. Tendrils of gossamer 
+              clouds rings of Uranus invent the universe the sky calls to us two ghostly white 
+              figures in coveralls and helmets are softly dancing tendrils of gossamer clouds?
+              Circumnavigated something incredible is waiting to be known made in the interiors 
+              of collapsing stars as a patch of light the carbon in our apple pies the carbon in 
+              our apple pies and billions upon billions upon billions upon billions upon billions 
+              upon billions upon billions.</span>
+          </div>
+
+          <footer>
+            <img src="linkedin-icon"/>
+            <img src="github-icon"/>
+          </footer>
+      </div>
     </div>
   );
 }
