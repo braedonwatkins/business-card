4d0c4c912efe399b09ba1ec86c702b0fd43545f4
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-23 15:55:25 -0400

    indentation consistency on exposition spans

--- src/App.js
+++ src/App.js
@@ -31,18 +31,18 @@ function App() {
           <div className="exposition">
             <h1>About</h1>
             <span>Rig Veda are creatures of the cosmos from which we spring with pretty stories 
-              for which there's little good evidence prime number Vangelis? Another world finite
-              but unbounded shores of the cosmic ocean invent the universe hearts of the stars two 
-              ghostly white figures in coveralls and helmets are softly dancing?.</span>
+            for which there's little good evidence prime number Vangelis? Another world finite
+            but unbounded shores of the cosmic ocean invent the universe hearts of the stars two 
+            ghostly white figures in coveralls and helmets are softly dancing?.</span>
           </div>
 
           <div className="exposition">
             <h1>Interests</h1>
             <span>Extraordinary claims require extraordinary evidence the ash of stellar alchemy
-              Rig Veda vanquish the impossible rich in mystery muse about. Tendrils of gossamer 
-              clouds rings of Uranus invent the universe the sky calls to us two ghostly white 
-              figures in coveralls and helmets are softly dancing tendrils of gossamer clouds?
-              </span>
+            Rig Veda vanquish the impossible rich in mystery muse about. Tendrils of gossamer 
+            clouds rings of Uranus invent the universe the sky calls to us two ghostly white 
+            figures in coveralls and helmets are softly dancing tendrils of gossamer clouds?
+            </span>
           </div>
 
           <footer>
